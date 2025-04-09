f933971db61cbc20fd87970208edc3d0d89c676c
Vinicius Borges <vinnepaul@gmail.com>
2025-04-09 10:35:14 -0300

    feat: adiciona pagina de edição

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
-import {ListComponent} from './features/list/list.component'
+import { ListComponent } from './features/list/list.component';
 import { CreateComponent } from './features/create/create.component';
 
-export const routes: Routes = [{
+export const routes: Routes = [
+  {
     path: '',
     component: ListComponent,
-},
-{
+  },
+  {
     path: 'create-product',
-    loadComponent: () => 
-        import('./features/create/create.component').then(
-            (m) => m.CreateComponent),
-},
+    loadComponent: () =>
+      import('./features/create/create.component').then(
+        (m) => m.CreateComponent
+      ),
+  },
+  {
+    path: 'edit-product',
+    loadComponent: () =>
+      import('./freatures/edit/edit.component').then((m) => m.EditComponent),
+  },
 ];
--- src/app/features/list/components/card/card.component.ts
+++ src/app/features/list/components/card/card.component.ts
@@ -1,6 +1,6 @@
-import { Component, computed, input } from '@angular/core';
-import { MatCardModule } from "@angular/material/card"
-import { MatButtonModule } from "@angular/material/button"
+import { Component, computed, EventEmitter, input, Output } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { Product } from '../../../../shared/interfaces/product.interface';
 
 @Component({
@@ -8,11 +8,12 @@ import { Product } from '../../../../shared/interfaces/product.interface';
   standalone: true,
   imports: [MatCardModule, MatButtonModule],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
   product = input.required<Product>();
 
-  productTitle = computed(() => this.product().title);
+  @Output() edit = new EventEmitter();
 
+  productTitle = computed(() => this.product().title);
 }
